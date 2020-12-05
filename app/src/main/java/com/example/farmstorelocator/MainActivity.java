eb9afa97820dd281bb2ec3cee150aca92cc4823e
Raphael <ic15b003@technikum-wien.at>
2020-12-05 16:18:39 +0100

    welcomepage added

--- app/src/main/java/com/example/farmstorelocator/MainActivity.java
+++ app/src/main/java/com/example/farmstorelocator/MainActivity.java
@@ -1,15 +1,30 @@
 package com.example.farmstorelocator;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
+
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar buyerToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        int test = 34;
+        setContentView(R.layout.test);
+
+        buyerToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(buyerToolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        return true;
+
     }
 }
