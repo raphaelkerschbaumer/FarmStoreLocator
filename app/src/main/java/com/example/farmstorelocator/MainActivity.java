2def4e8c289afdaea470b2b37254e08cb3b3d4e4
Raphael <ic15b003@technikum-wien.at>
2020-12-06 13:29:49 +0100

    shoplist & buyer menu überarbeitet

--- app/src/main/java/com/example/farmstorelocator/MainActivity.java
+++ app/src/main/java/com/example/farmstorelocator/MainActivity.java
@@ -1,16 +1,75 @@
 package com.example.farmstorelocator;
 
-import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
-public class MainActivity extends AppCompatActivity{
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
 
-    protected void onCreate(Bundle savedInstanceState){
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Toolbar buyerToolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.shoplist);
 
+        buyerToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(buyerToolbar);
 
+        //set list View
+        final ListView myList = (ListView) findViewById(R.id.listOfShops);
+        //final Button btn = (Button) findViewById(R.id.item1);
+
+        //list items
+        String[] items = new String[]{
+                "Entry 1",
+                "Entry 2",
+                "Entry 3",
+                "Entry 4",
+                "Entry 5",
+                "Entry 6",
+                "Entry 7",
+                "Entry 8",
+                "Entry 9",
+                "Entry 10",
+        };
+
+        //create a list from the string elements
+        final List<String> itemsList = new ArrayList<String>(Arrays.asList(items));
+
+        //create an Array Adapter from list
+        final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_selectable_list_item, itemsList);
+
+        // DataBind ListView with items from ArrayAdapter
+        myList.setAdapter(arrayAdapter);
+
+    //  btn.setOnClickListener(new View.OnClickListener() {
+    //      @Override
+    //      public void onClick(View v) {
+    //          // Add new Items to List
+    //          itemsList.add("Entry 11");
+    //          itemsList.add("Entry 12");
+    //          arrayAdapter.notifyDataSetChanged();
+    //      }
+    //  });
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        return true;
+
+    }
 }
